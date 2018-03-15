3eb25213cdb3ae6dbb610a94b8a4e2cf5f6ec7e1
Matt Tilley <mtilley255@gmail.com>
2018-03-15 10:45:32 -0500

    Refactored code for readability.

--- JS/primary.js
+++ JS/primary.js
@@ -1,6 +1,24 @@
 $(document).ready(function()
 {
-    $("#BTN02").mouseenter(function()
+    
+    
+    $("#BTN01").mouseenter(function()
+    {
+        $(this).hide();
+        $("#BTN01-HOV").show();
+    });
+    
+    $("#BTN01-HOV").mouseleave(function()
+    {
+        $(this).hide();
+        $("#BTN01").show();
+    });
+    
+    
+    
+    
+    
+    $("#BTN02").mouseenter(function()       /*CONTROLS FOR CLICK AREA #2*/
     {
         $(this).hide();
         $("#BTN02-HOV").show();
